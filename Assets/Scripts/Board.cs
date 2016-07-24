28b19b957b8cc51be15de9eedd09e74259ee43f3
proticjana <proticjana@gmail.com>
2016-07-24 17:01:48 +0200

    SetPieces added

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public enum BoardType { CHECKERED, CUSTOM };
-public enum PieceType { PAWN, KING, ROOK, BISHOP, KINGHT, QUEEN };
+public enum PieceType { PAWN, KING, ROOK, BISHOP, KINGHT, QUEEN, NONE };
 
 public class Board : MonoBehaviour {
     
     public GameObject blackField;
     public GameObject whiteField;
-    
-    public void BuildBoard (int width, int height, BoardType boardType, GameObject[][] customLayout = null) {
+
+    public int width;
+    public int height;
+
+    public GameObject rook;
+    public GameObject king;
+    public GameObject queen;
+    public GameObject bishop;
+    public GameObject pawn;
+    public GameObject knight;
+
+    public GameObject[,] board;
+
+    public void BuildBoard (int width, int height, BoardType boardType, GameObject[,] customLayout = null) {
+        this.width = width;
+        this.height = height;
+        board = new GameObject[height,width];
+
         Vector3 size = blackField.GetComponent<Renderer>().bounds.size;
         float sizeX = size.x;
         Debug.Log(size);
@@ -25,12 +41,11 @@ public class Board : MonoBehaviour {
                         field = (i + j) % 2 == 1 ? blackField : whiteField;
                         break;
                     case BoardType.CUSTOM:
-                        field = customLayout[i][j];
+                        field = customLayout[i, j];
                         break;
                     default:
                         break;
                 }
-
                 
                 GameObject instantiatedField = (GameObject)Instantiate(field,
                                                                        scaleFactor * (initialPosition + sizeX * new Vector3(j, i, 0)),
@@ -38,9 +53,33 @@ public class Board : MonoBehaviour {
 
                 instantiatedField.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
                 instantiatedField.transform.parent = gameObject.transform;
+
+                board[i, j] = instantiatedField;
             }
         }
     }
-    
+
+    public void SetPieces (PieceType[,] pieceLayout, bool isWhite) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                GameObject piece = null;
+
+                switch(pieceLayout[i, j]) {
+                    case PieceType.ROOK:
+                        piece = Instantiate(rook);
+                        break;
+                    case PieceType.KING:
+                        piece = Instantiate(king);
+                        break;
+                    default:
+                        continue;
+                }
+
+                piece.transform.parent = board[i, j].transform;
+                piece.transform.localPosition = new Vector3(0, 0, -1);
+                piece.transform.localScale = board[i, j].transform.localScale;
+            }
+        }
+    }
     
 }
