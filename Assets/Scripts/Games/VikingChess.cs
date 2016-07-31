3cb1bcaeaea4bdb936549caa356dc89bd9aceb28
jeremicmilan <milanjeremic@live.com>
2016-08-01 00:57:56 +0200

    Implemented attacking
    Added Attack function and implemented attacking by ROOK.
    Added custom board layout.
    Small fixes.

--- Assets/Scripts/Games/VikingChess.cs
+++ Assets/Scripts/Games/VikingChess.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class VikingChess : Game {
 
     public VikingChess ()
         : base(GameName.VIKING_CHESS, "Viking Chess", "", null) { }
 
-    void SetBoardAndPieces() {
-        GameObject boardObject = Object.Instantiate((GameObject)Resources.Load("Prefabs/Board", typeof(GameObject)));
+    void SetBoardAndPieces () {
+        GameObject boardObject = GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Board", typeof(GameObject)));
 
         board = boardObject.GetComponent<Board>();
         board.game = this;
 
-        board.BuildBoard(9, 9, BoardType.CHECKERED);
+        board.BuildBoard(9, 9, BoardType.CUSTOM,
+                         new FieldType[,] { { FieldType.ESCAPE,  FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.BLACK,   FieldType.BLACK,  FieldType.BLACK,   FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.ESCAPE  },
+                                            { FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.BLACK,  FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL },
+                                            { FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.WHITE,  FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL },
+                                            { FieldType.BLACK,   FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.WHITE,  FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.BLACK   },
+                                            { FieldType.BLACK,   FieldType.BLACK,   FieldType.WHITE,   FieldType.WHITE,   FieldType.CASTLE, FieldType.WHITE,   FieldType.WHITE,   FieldType.BLACK,   FieldType.BLACK   },
+                                            { FieldType.BLACK,   FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.WHITE,  FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.BLACK   },
+                                            { FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.WHITE,  FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL },
+                                            { FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.BLACK,  FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.NEUTRAL },
+                                            { FieldType.ESCAPE,  FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.BLACK,   FieldType.BLACK,  FieldType.BLACK,   FieldType.NEUTRAL, FieldType.NEUTRAL, FieldType.ESCAPE  }});
 
         board.SetPieces(new PieceType[,] { { PieceType.NONE, PieceType.NONE, PieceType.NONE, PieceType.ROOK, PieceType.ROOK, PieceType.ROOK, PieceType.NONE, PieceType.NONE, PieceType.NONE },
                                            { PieceType.NONE, PieceType.NONE, PieceType.NONE, PieceType.NONE, PieceType.ROOK, PieceType.NONE, PieceType.NONE, PieceType.NONE, PieceType.NONE },
@@ -42,5 +52,43 @@ public class VikingChess : Game {
 
     public override void StartTwoPlayer () {
         SetBoardAndPieces();
+    }
+
+    private bool Attack (Position position, Position direction, bool destroy = false) {
+        Piece opponentPiece = board.ValidPosition(position + direction) ? board.GetPiece(position + direction) : null;
+        if (opponentPiece && opponentPiece.isWhite != isWhitesTurn && board.ValidPosition(position + direction + direction)) {
+            Piece piece = board.GetPiece(position + direction + direction);
+            if (piece && piece.isWhite == isWhitesTurn) {
+                if (destroy) {
+                    GameObject.Destroy(opponentPiece.gameObject);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public override bool Attack (Move move, bool destroy = true) {
+        Piece piece = move.end.field.FindPiece();
+        piece = piece ? piece : move.start.field.FindPiece();
+
+        bool attacked = false;
+
+        if (piece.pieceType == PieceType.ROOK) {
+            if (Attack(move.end, new Position(1, 0, null), destroy)) {
+                attacked = true;
+            }
+            if (Attack(move.end, new Position(-1, 0, null), destroy)) {
+                attacked = true;
+            }
+            if (Attack(move.end, new Position(0, 1, null), destroy)) {
+                attacked = true;
+            }
+            if (Attack(move.end, new Position(0, -1, null), destroy)) {
+                attacked = true;
+            }
+        }
+
+        return attacked;
     }
 }
