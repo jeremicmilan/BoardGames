4316ef9a6c5bbe83462316ba5c4f208235a37c54
proticjana <proticjana@gmail.com>
2016-08-04 22:47:44 +0200

    Moved everything from GameMenu to UI

--- Assets/Scripts/GameIcon.cs
+++ Assets/Scripts/GameIcon.cs
@@ -4,13 +4,12 @@ using System.Collections;
 public class GameIcon : MonoBehaviour {
 
     public UI ui;
-    public GameMenu gameMenu;
 
     public GameName gameName;
 
     public void OnClick () {
-        ui.setActiveUI(gameMenu.gameObject.GetComponent<Canvas>());
+        ui.setActiveUI(ui.gameMenu);
 
-        gameMenu.SetGame(gameName);
+        ui.SetGame(gameName);
     }
 }
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public enum GameName { VIKING_CHESS, REVERSI, CHECKERS };
 
 public class UI : MonoBehaviour {
+    public Game game;
 
     public Canvas mainMenu;
     public Canvas gameMenu;
     public Canvas gameUI;
+
+    public Text gameTitle;
 
     public void setActiveUI (Canvas ui) {
         mainMenu.enabled = false;
@@ -27,5 +31,38 @@ public class UI : MonoBehaviour {
         setActiveUI(mainMenu);
     }
 
+    public void SetGame (GameName gameName) {
+        switch (gameName) {
+            case GameName.VIKING_CHESS:
+                game = new VikingChess();
+                break;
+            case GameName.REVERSI:
+                game = new Reversi();
+                break;
+            case GameName.CHECKERS:
+                game = new Checkers();
+                break;
+            default:
+                break;
+        }
+
+        gameTitle.text = game.name;
+    }
+
+    public void StartSinglePlayer () {
+        setActiveUI(gameUI);
+
+        game.StartSinglePlayer();
+    }
+
+    public void StartTwoPlayer () {
+        setActiveUI(gameUI);
+
+        game.StartTwoPlayer();
+    }
+
+    public void Undo () {
+        game.board.Undo();
+    }
 }
 
