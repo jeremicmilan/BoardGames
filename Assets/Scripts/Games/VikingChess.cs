70c5e1b204394e549d739f1e121c1124e2ff9790
proticjana <proticjana@gmail.com>
2016-08-04 22:51:01 +0200

    Added and implemented CanMoveTo. CheckForEnd and CheckForPieceEvolve
    Fixed PieceType enum usage.
    We do not destroy pieces anymore, we move them to graveyard.
    Added helper functions DidKingEscape, IsSurrounded and IsKingCaptured
    for CheckForEnd.

--- Assets/Scripts/Games/VikingChess.cs
+++ Assets/Scripts/Games/VikingChess.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 
 public class VikingChess : Game {
+
+    List<Position> visitedPositions = new List<Position>();
 
     public VikingChess ()
         : base(GameName.VIKING_CHESS, "Viking Chess", "", null) { }
@@ -12,6 +15,7 @@ public class VikingChess : Game {
 
         board = boardObject.GetComponent<Board>();
         board.game = this;
+        board.graveyard = GameObject.Find("Graveyard");
 
         board.BuildBoard(9, 9, BoardType.CUSTOM,
                          new FieldType[,] { { FieldType.ESCAP, FieldType.NEUTR, FieldType.NEUTR, FieldType.BLACK, FieldType.BLACK, FieldType.BLACK, FieldType.NEUTR, FieldType.NEUTR, FieldType.ESCAP },
@@ -24,23 +28,23 @@ public class VikingChess : Game {
                                             { FieldType.NEUTR, FieldType.NEUTR, FieldType.NEUTR, FieldType.NEUTR, FieldType.BLACK, FieldType.NEUTR, FieldType.NEUTR, FieldType.NEUTR, FieldType.NEUTR },
                                             { FieldType.ESCAP, FieldType.NEUTR, FieldType.NEUTR, FieldType.BLACK, FieldType.BLACK, FieldType.BLACK, FieldType.NEUTR, FieldType.NEUTR, FieldType.ESCAP } });
 
-        board.SetPieces(new PieceType[,] { { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.CH_ROOK, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
+        board.SetPieces(new PieceType[,] { { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.VK_ROOK, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
+                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
                                            { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK },
-                                           { PieceType.CH_ROOK, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.CH_ROOK },
-                                           { PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK },
+                                           { PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK },
+                                           { PieceType.VK_ROOK, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.VK_ROOK },
+                                           { PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK },
                                            { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.CH_ROOK, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE } },
+                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
+                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.VK_ROOK, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE } },
                         false);
         board.SetPieces(new PieceType[,] { { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
                                            { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.CH_ROOK, PieceType.VK_KING, PieceType.CH_ROOK, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
-                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.CH_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
+                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
+                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
+                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.VK_ROOK, PieceType.VK_KING, PieceType.VK_ROOK, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE },
+                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
+                                           { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.VK_ROOK, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
                                            { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE },
                                            { PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE, PieceType.AL_NONE } },
                         true);
@@ -54,17 +58,23 @@ public class VikingChess : Game {
         SetBoardAndPieces();
     }
 
-    private bool Attack (Position position, Position direction, bool destroy = false) {
+    private bool Attack (Move move, Position direction, bool destroy = false) {
+        Position position = move.end;
         Piece opponentPiece = board.ValidPosition(position + direction) ? board.GetPiece(position + direction) : null;
-        if (opponentPiece && opponentPiece.isWhite != isWhitesTurn && board.ValidPosition(position + direction + direction)) {
-            Piece piece = board.GetPiece(position + direction + direction);
-            if (piece && piece.isWhite == isWhitesTurn) {
-                if (destroy) {
-                    GameObject.Destroy(opponentPiece.gameObject);
+        if (opponentPiece && opponentPiece.isWhite != isWhitesTurn && opponentPiece.pieceType != PieceType.VK_KING) {
+            Position allyPosition = position + 2 * direction;
+            if (board.ValidPosition(allyPosition)) {
+                Piece piece = board.GetPiece(allyPosition);
+                if (piece && piece.isWhite == isWhitesTurn) {
+                    if (destroy) {
+                        move.eatenPieces.Add(opponentPiece);
+                        board.SendToGraveyard(opponentPiece);
+                    }
+                    return true;
                 }
-                return true;
             }
         }
+        
         return false;
     }
 
@@ -74,21 +84,96 @@ public class VikingChess : Game {
 
         bool attacked = false;
 
-        if (piece.pieceType == PieceType.CH_ROOK) {
-            if (Attack(move.end, new Position(1, 0, null), destroy)) {
+        if (piece.pieceType == PieceType.VK_ROOK) {
+            if (Attack(move, new Position(1, 0, null), destroy)) {
                 attacked = true;
             }
-            if (Attack(move.end, new Position(-1, 0, null), destroy)) {
+            if (Attack(move, new Position(-1, 0, null), destroy)) {
                 attacked = true;
             }
-            if (Attack(move.end, new Position(0, 1, null), destroy)) {
+            if (Attack(move, new Position(0, 1, null), destroy)) {
                 attacked = true;
             }
-            if (Attack(move.end, new Position(0, -1, null), destroy)) {
+            if (Attack(move, new Position(0, -1, null), destroy)) {
                 attacked = true;
             }
         }
 
         return attacked;
+    }
+
+    public override bool CanMoveTo (int x, int y, PieceType pieceType = PieceType.AL_NONE) {
+        Field field = board.GetField(x, y);
+        FieldType fieldType = field.fieldType;
+        Piece piece = field.FindPiece();
+        return (!piece && fieldType != FieldType.CASTL && fieldType != FieldType.ESCAP) ||
+               (!piece && pieceType == PieceType.VK_KING);
+    }
+
+    private bool DidKingEscape () {
+        Piece piece = board.GetField(0, 0).FindPiece();
+        if (piece && piece.pieceType == PieceType.VK_KING) {
+            return true;
+        }
+        piece = board.GetField(board.width - 1, 0).FindPiece();
+        if (piece && piece.pieceType == PieceType.VK_KING) {
+            return true;
+        }
+        piece = board.GetField(0, board.height - 1).FindPiece();
+        if (piece && piece.pieceType == PieceType.VK_KING) {
+            return true;
+        }
+        piece = board.GetField(board.width - 1, board.height - 1).FindPiece();
+        if (piece && piece.pieceType == PieceType.VK_KING) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool IsSurrounded (Position position) {
+        if (visitedPositions.Contains(position)) {
+            return true;
+        }
+        visitedPositions.Add(position);
+
+        Piece piece = board.GetField(position).FindPiece();
+
+        if (!board.ValidPosition(position)) {
+            return true;
+        }
+
+        if (!piece) {
+            return false;
+        } else if (piece.isWhite == false) {
+            return true;
+        } else {
+            return IsSurrounded(position + new Position(1, 0, null)) &&
+                   IsSurrounded(position + new Position(0, 1, null)) &&
+                   IsSurrounded(position + new Position(-1, 0, null)) &&
+                   IsSurrounded(position + new Position(0, -1, null));
+        }
+    }
+
+    private bool IsKingCaptured () {
+        visitedPositions.Clear();
+
+        return IsSurrounded(board.FindPiece(PieceType.VK_KING).position);
+    }
+
+    public override bool CheckForEnd (ref bool whiteWon) {
+        if (DidKingEscape()) {
+            whiteWon = true;
+            return true;
+        }
+        if (IsKingCaptured()) {
+            whiteWon = false;
+            return true;
+        }
+        return false;
+    }
+
+    public override bool CheckForPieceEvolve () {
+        return false;
     }
 }
